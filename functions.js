e0cefbbdb3793c70cd798aee27e87dceac911a26
Cristian Vaca <civaca7@gmail.com>
2021-12-08 20:47:52 -0500

    Update

--- functions.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  
-  $("ul").addClass("list-group");
-  $("li").addClass("list-group-item ");
-     $("h6").addClass("list-group-item-heading lead");
-      $(".years").addClass("list-group-item-text");
-
-   });
